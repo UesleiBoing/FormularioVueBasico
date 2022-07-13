ee15e9756d2a34981f60eab57f30702d6d948237
Afonso <boingfueslei@hotmail.com>
2022-07-13 20:24:34 -0300

    Ajustado

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const porta = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/dist/'));
 
@@ -7,6 +8,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(3000, () => {
+app.listen(porta, () => {
   console.log('Example app listening on port 3000!');
 });
