e4432bd684d57fd3f83814202c572f190be54c36
Afonso <boingfueslei@hotmail.com>
2022-07-14 21:48:31 -0300

    Segue

--- test/testeUnitario.spec.js
+++ test/testeUnitario.spec.js
@@ -2,7 +2,7 @@ import {soma} from './matematicas';
 
 test('1 + 1 = 2', 
     () => {
-        expect(soma(1, 2)).toBe(2);
+        expect(soma(1, 1)).toBe(2);
     }, 
     () => {
         expect(soma(1, 1)).not.toBe(3);
